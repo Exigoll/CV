92df4bbe48b07d7125b998fe4be4425e5c4e00a8
Exigoll <Exigoll@bk.ru>
2021-12-22 15:37:06 +0500

    feat: style scroll bar

---
+++ js/index.js
@@ -0,0 +1,5 @@
+// Navigation bar effecs on scroll
+window.addEventListener('scroll', function() {
+  const header = this.document.querySelector('header')
+  header.classList.toggle('sticky', window.scrollY > 0)
+})
