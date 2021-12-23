18757f4f82cc6ab4a74f799a155c93056f1f5d0b
Exigoll <Exigoll@bk.ru>
2021-12-23 14:54:41 +0500

    feat: sections services & portfolio

--- js/index.js
+++ js/index.js
@@ -3,3 +3,50 @@ window.addEventListener('scroll', function() {
   const header = this.document.querySelector('header')
   header.classList.toggle('sticky', window.scrollY > 0)
 })
+
+// Services section - Modal window
+const serviceModals = document.querySelectorAll('.service-modal')
+const learnmoreBtns = document.querySelectorAll('.learn-more-btn')
+const modalCloseBtns = document.querySelectorAll('.modal-close-btn')
+
+const modal = (modalClick) => {
+  serviceModals[modalClick].classList.add('active')
+}
+
+learnmoreBtns.forEach((learmoreBtn, i) => {
+  learmoreBtn.addEventListener('click', () => {
+    modal(i)
+  })
+})
+
+modalCloseBtns.forEach((modalCloseBtn) => {
+  modalCloseBtn.addEventListener('click', () => {
+    serviceModals.forEach((modalView) => {
+      modalView.classList.remove('active')
+    })
+  })
+})
+
+
+// Portfolio section - MModal window
+const portfolioModals = document.querySelectorAll('.portfolio-model')
+const imgCards = document.querySelectorAll('.img-card')
+const portfolioCloseBtns = document.querySelectorAll('.portfolio-close-btn')
+
+const portfolioModal = (modalClick) => {
+  portfolioModals[modalClick].classList.add('active')
+}
+
+imgCards.forEach((imgCard, i) => {
+  imgCard.addEventListener('click', () => {
+    portfolioModal(i)
+  })
+})
+
+portfolioCloseBtns.forEach((portfolioCloseBtn) => {
+  portfolioCloseBtn.addEventListener('click', () => {
+    portfolioModals.forEach((portfolioModalView) => {
+      portfolioModalView.classList.remove('active')
+    })
+  })
+})
